869db23be182348d20d62a0233e0a09ac5a69422
Atle Hansson <hansson.atle@gmail.com>
2021-01-24 02:25:44 -0500

    Start color themes (WIP)

--- _public/assets/js/scripts.js
+++ _public/assets/js/scripts.js
@@ -1,6 +1,39 @@
 (() => {
   
-  // Documentation menu (handheld)
+  // Theme Switcher
+  const themeSwitch = document.querySelector('.theme-switch input[type="checkbox"]')
+  const sun = document.querySelector('.light')
+  const moon = document.querySelector('.dark')
+  if (colorMode.preferredTheme === 'dark') {
+    themeSwitch.checked = true
+    moon.classList.remove('hide')
+    localStorage.setItem('theme', 'dark')
+  }
+  if (colorMode.preferredTheme === 'light') {
+    themeSwitch.checked = false
+    sun.classList.remove('hide')
+    localStorage.setItem('theme', 'light')
+  }
+  const switchTheme = (e) => {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark')
+      moon.classList.remove('hide')
+      moon.classList.add('fade-in')
+      sun.classList.add('hide')
+      sun.classList.remove('fade-in')
+      localStorage.setItem('theme', 'dark')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light')
+      moon.classList.add('hide')
+      moon.classList.remove('fade-in')
+      sun.classList.remove('hide')
+      sun.classList.add('fade-in')
+      localStorage.setItem('theme', 'light')
+    }
+  }
+  themeSwitch.addEventListener('change', switchTheme, false)
+  
+  // Documentation Menu (handheld)
 
   if (!document.querySelector('.docs-menu')) {
     return
