eb15879619939c3f4db206ad1a0a961455d473f2
Atle Hansson <hansson.atle@gmail.com>
2021-01-25 20:57:50 -0500

    Fix conflicts for mobile menus js

--- _public/assets/js/scripts.js
+++ _public/assets/js/scripts.js
@@ -1,13 +1,12 @@
 (() => {
-  
+
   // Color Theme Switcher
-  
   const themeSwitch = document.querySelectorAll('.theme-switch input[type="checkbox"]')
-  const toggle1 = document.querySelector('.toggle1')
-  const toggle2 = document.querySelector('.toggle2')
+  const toggle1 = document.querySelector('.desktop-toggle')
+  const toggle2 = document.querySelector('.mobile-toggle')
+  const toggle3 = document.querySelector('.docs-toggle')
   const sun = document.querySelectorAll('.light')
   const moon = document.querySelectorAll('.dark')
-
   const swapIcons = (el1, el2) => {
     el1.forEach((el) => {
       el.classList.remove('hide')
@@ -18,6 +17,7 @@
       el.classList.remove('fade-in')
     })
   }
+  
   if (colorMode.preferredTheme === 'dark') {
     themeSwitch.forEach(el => el.checked = true)
     moon.forEach(el => el.classList.remove('hide'))
@@ -27,6 +27,7 @@
     sun.forEach(el => el.classList.remove('hide'))
     localStorage.setItem('theme', 'light')
   }
+
   const switchTheme = (e) => {
     if (e.target.checked) {
       document.documentElement.setAttribute('data-theme', 'dark')
@@ -40,55 +41,29 @@
       colorMode.meta.content = '#23004b'
     }
   }
-  toggle1.addEventListener('change', switchTheme, false)
-  toggle2.addEventListener('change', switchTheme, false)
-  
+
+  const toggles = [toggle1, toggle2, toggle3]
+  const listen = (elem) => elem.map(el => (el !== null) ? el.addEventListener('change', switchTheme, false) : false)
+  listen(toggles)
+
   // Documentation Menu (handheld)
-
-  if (!document.querySelector('.docs-menu')) {
-    return
-  } else {
-    const trigger = document.querySelector('.menu-trigger')
-    const docsMenu = document.querySelector('.docs-menu')
-    const close = document.querySelector('.close-x')  
-    trigger.addEventListener('click', () => {
-      docsMenu.classList.toggle('show')
-    })
-    close.addEventListener('click', () => {
-      docsMenu.classList.toggle('show')
-    })
-    document.addEventListener('click', (e) => {
-      const clickInside = docsMenu.contains(e.target) || close.contains(e.target) || trigger.contains(e.target)
-      if (!clickInside) {
-        docsMenu.classList.remove('show')
-      }
-    })
-  }
-})();
-  
-(() => {
-  
-  // Regular menu (handheld)
-
-  if (!document.querySelector('.mobile-menu')) {
-    return
-  } else {
+  if (document.querySelector('.docs-menu') || document.querySelector('.mobile-menu')) {
     const trigger = document.querySelector('.menu-trigger')
     const mobileMenu = document.querySelector('.mobile-menu')
+    const docsMenu = document.querySelector('.docs-menu')
     const close = document.querySelector('.close-x')
 
-    trigger.addEventListener('click', () => {
-      mobileMenu.classList.toggle('show')
-    })
-    close.addEventListener('click', () => {
-      mobileMenu.classList.toggle('show')
-    })
-    document.addEventListener('click', (e) => {
-      const clickInside = mobileMenu.contains(e.target) || close.contains(e.target) || trigger.contains(e.target)
-      if (!clickInside) {
-        mobileMenu.classList.remove('show')
-      }
-    })
+    const toggleMenu = (el) => {
+      trigger.addEventListener('click', () => el.classList.toggle('show'))
+      close.addEventListener('click', () => el.classList.toggle('show'))
+      document.addEventListener('click', (e) => {
+        const clickInside = el.contains(e.target) || close.contains(e.target) || trigger.contains(e.target)
+        if (!clickInside) {
+          el.classList.remove('show')
+        }
+      })
+    }
+    (mobileMenu !== null) ? toggleMenu(mobileMenu) : toggleMenu(docsMenu)
   }
 })();
 
