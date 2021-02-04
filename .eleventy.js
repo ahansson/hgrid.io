5722d89acc54e892a41388c5c3f1ca1e5dd073f5
Atle Hansson <hansson.atle@gmail.com>
2021-02-04 04:24:31 -0500

    Move assets folder. WIP: refactor css/scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = (eleventyConfig) => {
   
-  eleventyConfig.addPassthroughCopy('assets/css/styles.min.css')
-  eleventyConfig.addPassthroughCopy('assets/css/styles.min.map')
-  eleventyConfig.addPassthroughCopy('assets/img/**')
+  eleventyConfig.addWatchTarget("/src/_assets/scss/main.scss");
+  eleventyConfig.addPassthroughCopy('/src/_assets/css/style.min.css')
+  eleventyConfig.addPassthroughCopy('/src/_assets/css/style.min.css.map')
+  eleventyConfig.addPassthroughCopy('/src/_assets/css/style.css')
+  eleventyConfig.addPassthroughCopy('/src/_assets/img/')
+  eleventyConfig.addPassthroughCopy('/src/_assets/js/')
 
   return {
     passthroughFileCopy: true,
---
+++ src/_assets/js/transition.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', () => {
+  document.querySelector('.transition').classList.add('vanish')
+})
